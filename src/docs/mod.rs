69e1107180c9ad4e17709fa312c27ffa0aecd1ac
Wren Powell <wrenp@duck.com>
2022-12-22 22:39:03 -0500

    Rename module in docs

--- src/docs/mod.rs
+++ src/docs/mod.rs
@@ -8,7 +8,7 @@ This is the xpct user documentation. If you're new here, check out the
 #![cfg(docsrs)]
 
 pub mod cargo_features;
-pub mod list_of_matchers;
+pub mod matcher_list;
 pub mod tutorial;
 pub mod writing_formatters;
 pub mod writing_matchers;
