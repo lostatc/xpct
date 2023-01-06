979542428b0b70c770505f6f636b6b00fd883a06
Wren Powell <wrenp@duck.com>
2023-01-06 08:40:46 -0500

    Improve error message for `be_empty` matcher

--- src/format/len.rs
+++ src/format/len.rs
@@ -3,7 +3,7 @@ use std::fmt;
 use crate::core::Matcher;
 use crate::matchers::{HaveLenMatcher, Len};
 
-use super::MismatchFormat;
+use super::{MessageFormat, MismatchFormat};
 
 /// Succeeds when the actual value has the given length.
 ///
@@ -45,7 +45,7 @@ where
 {
     Matcher::simple(
         HaveLenMatcher::new(0),
-        MismatchFormat::new("to be empty", "to not be empty"),
+        MessageFormat::new("Expected this to be empty", "Expected this to not be empty"),
     )
 }
 
